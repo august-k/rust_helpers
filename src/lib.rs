890de8a5d55ea2e9bacd6b3d62eb3ed823e97873
august-k <august.kaplan@gmail.com>
2021-05-04 21:59:40 -0400

    Update lib.rs
    positions are now tuples so they'll be returned to python-sc2 as tuples to better match Point2

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use pyo3::prelude::*;
 #[pymodule]
 fn rust_helpers(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "get_neighbors")]
-    fn get_neighbors(_py: Python, raw_point: [f32; 2]) -> Vec<(i32, i32)> {
+    fn get_neighbors(_py: Python, raw_point: (f32, f32)) -> Vec<(i32, i32)> {
         // Return the 8 neighboring coordinates of point
         let mut neighbors: Vec<(i32, i32)> = Vec::with_capacity(8);
         /* Uses 0 to 3 and -1 because I'm not sure if Rust allows
         signed ints as iteration values */
-        let point = [raw_point[0] as i32, raw_point[1] as i32];
+        let point: [i32; 2] = [raw_point.0 as i32, raw_point.1 as i32];
         for i in 0..3 {
             for j in 0..3 {
                 // skips what would be +0, +0 (the initial point)
@@ -24,8 +24,8 @@ fn rust_helpers(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "index_of_closest")]
     fn index_of_closest(
         _py: Python,
-        unit_positions: Vec<[f32; 2]>,
-        target_position: [f32; 2],
+        unit_positions: Vec<(f32, f32)>,
+        target_position: (f32, f32)
     ) -> usize {
         let mut closest_index: usize = 0;
         let mut closest_distance: f32 = 9999.9;
@@ -39,8 +39,9 @@ fn rust_helpers(_py: Python, m: &PyModule) -> PyResult<()> {
         return closest_index;
     }
 
-    fn get_squared_distance(p1: [f32; 2], p2: [f32; 2]) -> f32 {
-        return f32::powf(p1[0] - p2[0], 2.0) + f32::powf(p1[1] - p2[1], 2.0);
+
+    fn get_squared_distance(p1: (f32, f32), p2: (f32, f32)) -> f32 {
+        return f32::powf(p1.0 - p2.0, 2.0) + f32::powf(p1.1 - p2.1, 2.0)
     }
 
     Ok(())
